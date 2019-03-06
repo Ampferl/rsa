4a6680e58783505ea8e865bf53455ab65651e7b6
Jonas <Jonas@chaepy.net>
2019-03-06 07:32:18 +0100

    Basic RSA En-/Decryption

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-rsa
+# RSA Encryption 
+## How it works
+We have to choose to prime numbers. \
+For example we will use `p=11` and `q=13`.
+#### RSA-Modul
+Then we calculate the RSA-Modul N. We can do this with `N=p*q=11*13=143` \
+Then we need φ for the private Key. `φ(N)=(p-1)*(q-1)=120`\
+#### Public-Key
+The next step is to choose an public key (it must be a random and also prime number), for example i choose `e=23`
+#### Private Key
+Now we must calculate the private key with the Extended Euclidean algorithm. The result was `d=47`
+#### Encrypt
+Calculation:\
+`c = m^e (mod N)`\
+\
+Example:\
+`2 = 7^23 (mod 143)`
+#### Decrypt
+Calculation:\
+`m = c^d (mod N)`\
+\
+Example:\
+`7 = 2^47 (mod 143)`
+#### Sign
+Calculation:\
+`c = m^d (mod N)`\
+\
+Example:\
+`28 = 7^47 (mod 143)`
+#### Verify
+Calculation:\
+`m = c^e (mod N)`\
+\
+Example:\
+`7 = 28^23 (mod 143)`
+## Quick Guide
+Show your RSA public_key: get_publickey();´ \
+Show your RSA private_key: get_privatekey();´\
+\
+Encrypt a message:
+```
+encrypt_msg(msg,pkey,noncee); 
+/*
+msg(Here you can write your Message), 
+pkey (Here you need the public key of the reciever), 
+nonce (Here you need the nonce of the reciever)
+*/
+```
+\
+Decrypt a message:
+```
+decrypt_msg(msg,pkey,noncee); 
+/*
+msg(Here you can write enrypted Message), 
+pkey (Here you need your private key), 
+nonce (Here you need your nonce)
+*/
+```
+
---
+++ crypter.js
@@ -0,0 +1,61 @@
+
+
+var p = 131;
+	var q = 257;
+	var nonce = new Decimal(p * q);
+	var pN = (p - 1) * (q - 1);
+	var public_key = new Decimal("137");
+	var private_key = new Decimal(Euclid_gcd(public_key,pN));
+	
+
+	// e = public_key 
+	// d = private_key (Dont give your private key to other people!)
+	// N = RSA-Module
+	// φ(N) = RSA-Module-φ for the calculation of the private_key
+	
+function encrypt_msg(msg,pkey,noncee){
+	var txt = msg;
+	
+	var output_stuff = txt.split("");
+	
+
+	var i = 0;
+	var resulttxt = "";
+	while(output_stuff[i]){
+		var hex = output_stuff[i].charCodeAt(0).toString(16);
+		var hexsplited = hex.split("");
+		var charinhexa = hexatronin(hexsplited[0]) + hexatronin(hexsplited[1]) + Math.floor((Math.random() * 9) + 1);
+		var encrypted = Decimal.mod(Decimal.pow(charinhexa,pkey),noncee);
+		var resulttxt = resulttxt + " " + encrypted;
+		i++;
+	}
+	return resulttxt;
+}
+
+function decrypt_msg(msg,pkey,nonced){
+	var txt = msg;
+	
+	var output_stuff = txt.split(" ");
+	
+
+	var i = 0;
+	var resulttxt = "";
+	while(output_stuff[i]){
+		var decrypted = Decimal.mod(Decimal.pow(output_stuff[i],pkey),nonced);
+		
+		var resdecrypted = decrypted+"";
+		var resdecrypted = resdecrypted.substr(0, resdecrypted.length - 1);
+		var resdecrypted = hexatronout(resdecrypted);
+		var resulttxt = resulttxt + "" + resdecrypted;
+		i++;
+	}
+	return hex_to_ascii(resulttxt);
+}
+	
+function get_privatekey(){
+	document.write("<br>Private Key: " + private_key);
+}
+
+function get_publickey(){
+	document.write("<br>Public Key: " + public_key);
+}
